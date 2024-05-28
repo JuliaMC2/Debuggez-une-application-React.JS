3af5690859923696614b1f3439569f5c02d94fb1
CedricMolendiCoste <juliacaudal@gmail.com>
2024-05-28 16:54:52 +0200

    FIX : Slider -> ordre des événements

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -8,7 +8,7 @@ const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+    new Date(evtA.date) < new Date(evtB.date) ? -1 : 0
   );
   const nextCard = () => {
     setTimeout(
@@ -24,7 +24,7 @@ const Slider = () => {
       {byDateDesc?.map((event, idx) => (
         <>
           <div
-            key={event.title}
+            key={event.id}
             className={`SlideCard SlideCard--${
               index === idx ? "display" : "hide"
             }`}
